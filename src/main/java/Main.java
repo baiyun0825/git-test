a4dcb93c76618732447224e808b80f362190370c
hjj <1017673693@qq.com>
2022-05-29 13:18:49 +0800

    hot-fix -first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第二次提交");
+        System.out.println("hot fix -----");
     }
 }
